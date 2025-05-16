cc1c83bab3e372cc3318decfe3e02da5d5f574c5
Eunyoung-Choe <ceytown@student.hywoman.ac.kr>
2025-05-16 14:55:40 +0900

    contents 영역 height 반응형 조정 필요/attend 파일들 확인

--- src/main/squarereact/src/index.js
+++ src/main/squarereact/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Main from './components/Main';
+// import Main from './components/Main';
+import RouterMain from './router/RouterMain';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <Main />
+  <React.StrictMode>
+    <RouterMain />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
