77bc20c827fcf5959bd95b6d39271df5bf6b987f
[sujinpsujinp] <sujinp038@naver.com>
2025-06-05 14:46:42 +0900

    종합평가 페이지 완료

--- src/main/java/com/example/classes/jpa/ClassUsersRepository.java
+++ src/main/java/com/example/classes/jpa/ClassUsersRepository.java
@@ -2,6 +2,7 @@ package com.example.classes.jpa;
 
 import com.example.classes.entity.ClassUsersEntity;
 import com.example.classes.entity.ClassesEntity;
+import com.example.user.dto.StudentDto;
 import com.example.user.entity.StudentsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,12 @@ public interface ClassUsersRepository extends JpaRepository<ClassUsersEntity, In
     /** classId에 해당하는 학생 목록 조회 - 시간표 페이지에서 사용*/
     @Query("SELECT cu.student.studentId FROM ClassUsersEntity cu WHERE cu.classEntity.classId = :classId")
     List<Integer> findStudentIdsByClassId(int classId);
+
+    /** userId로 student 이름 조회 - 종합평가 등록 페이지에서 사용*/
+    @Query("SELECT new com.example.user.dto.StudentDto(s.studentId, u.name) " +
+            "FROM ClassUsersEntity cu " +
+            "JOIN cu.student s " +
+            "JOIN s.user u " +
+            "WHERE cu.classEntity.classId = :classId")
+    List<StudentDto> findStudentDtoByClassId(int classId);
 }
